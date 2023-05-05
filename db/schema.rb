b8be4fe1925e98ce44833766760cc3fed1da060d
dadadon <davianberoni@yahoo.com>
2023-05-04 23:01:16 -0500

    Join payment and category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_035718) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_035921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_035718) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_payments", id: false, force: :cascade do |t|
+    t.bigint "payment_id", null: false
+    t.bigint "category_id", null: false
   end
 
   create_table "payments", force: :cascade do |t|
