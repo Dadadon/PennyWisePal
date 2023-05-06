7a26bdc60028f47ebd81e7d899851d4f856f4038
dadadon <davianberoni@yahoo.com>
2023-05-05 21:47:38 -0500

    Update payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,32 +1,46 @@
-# app/controllers/payments_controller.rb
 class PaymentsController < ApplicationController
-    before_action :set_category
-  
-    def index
-      @payments = @category.payments
-    end
-  
-    def new
-      @payment = @category.payments.new
-    end
-  
-    def create
-      @payment = @category.payments.new(payment_params)
-      if @payment.save
-        redirect_to category_payments_path(@category), notice: "Payment created successfully."
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def set_category
-      @category = Category.find(params[:category_id])
-    end
-  
-    def payment_params
-      params.require(:payment).permit(:name, :amount, category_ids: [])
-    end
+before_action :set_category
+before_action :authenticate_user!
+
+def index
+end
+
+def new
+  @current_user = current_user
+  @payment = Payment.new
+  @categories = current_user.categories
+end
+
+def create
+  @current_user = current_user
+  @category = Category.find(params[:category_id])
+  @payment = Payment.new(payment_params)
+  @payment.user = @current_user
+  @payment.category_id = @category.id 
+
+  if @payment.save
+    redirect_to category_path(@category)
+  else
+    render :new
   end
-  
+end
+
+
+def destroy
+  @payment = Payment.find(params[:id]).destroy
+  respond_to do |format|
+    format.html { redirect_to category_path(@category), notice: 'payment deleted successfully' }
+  end
+end
+
+private
+
+def set_category
+  @category = current_user.categories.find(params[:category_id])
+end
+
+def payment_params
+  params.require(:payment).permit(:name, :amount, :category_id)
+end
+
+end
