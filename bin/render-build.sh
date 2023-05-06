6deb6d40ca4dd9fba62b4dca468ff9d1d6cee0f6
dadadon <davianberoni@yahoo.com>
2023-05-06 03:09:23 -0500

    Update files

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-bundle config --global frozen 0
-bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
