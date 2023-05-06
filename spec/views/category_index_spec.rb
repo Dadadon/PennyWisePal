e87e17ddf0734a8d829a903d92a9fcd4731b78d5
dadadon <davianberoni@yahoo.com>
2023-05-05 23:23:13 -0500

    Add test for views

---
+++ spec/views/category_index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'categories/index', type: :feature do
+  include Warden::Test::Helpers
+
+  before do
+    @user = User.create!(name: 'John Doe', email: 'johndoe@example.com', password: 'password')
+    @user.skip_confirmation!
+    @user.save
+    login_as(@user, scope: :user)
+    @category = Category.create!(name: 'Food', icon: 'https://media.istockphoto.com/id/1481730340/photo/gray-hair-man-cooking-healthy-meal-in-modern-apartment.jpg?s=612x612&w=is&k=20&c=AL4fSbJx9QJ8iEsETWT1hjsIigsfQ8HfmNerlkpLveE=', user: @user)
+    @payment = Payment.create(user: @user, name: 'Rent', amount: 1000, category_id: @category.id)
+    visit categories_path
+  end
+
+  it 'displays App name' do
+    expect(page).to have_content('CATEGORIES')
+  end
+
+  it 'displays category name' do
+    expect(page).to have_content(@category.name)
+  end
+
+  it 'displays updated_at date' do
+    expect(page).to have_content(@category.updated_at.strftime('%d %b %Y'))
+  end
+
+  it 'displays an "ADD CATEGORY" button' do
+    expect(page).to have_link('ADD CATEGORY', href: new_category_path)
+  end
+end
