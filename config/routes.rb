94beab82503c2cabc91567419db860e330b833b9
dadadon <davianberoni@yahoo.com>
2023-05-05 21:45:23 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'pages#splash'
   get '/categories', to: 'categories#index', as: 'categories'
   resources :categories do
-    resources :payments, only: [:index, :new, :create]
+    resources :payments
   end
   
 end
